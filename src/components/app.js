02c336a15202cdf3d12f2f67b6ebfa75dae83849
Van <1137623688@qq.com>
2020-04-25 17:16:52 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ import Header from './header';
 import Home from '../routes/home';
 import Blogs from '../routes/blogs';
 import Blog from '../routes/blog';
+import Members from '../routes/members'
 import Contact from '../routes/contact';
 import Members from '../routes/members';
 import Others from '../routes/others'
@@ -14,17 +15,25 @@ import Menu from '../routes/menu'
 import Notice from '../routes/notice'
 import ContactSuccess from '../routes/contact-success';
 import NotFoundPage from '../routes/notfound';
-
+/*
+App:网页函数所在，网页的一些预设
+*/
 export default class App extends Component {
 
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
+
+	/*
+	handleRoute:跳转处理函数
+	*/
 	handleRoute = e => {
 		this.currentUrl = e.url;
 	};
-
+	/*
+	设置router
+	*/
 	render(props) {
 		return (
 			<Provider value={props}>
@@ -36,9 +45,13 @@ export default class App extends Component {
 						<Blog path="/blog/:name" />
 						<Members path="/members/"/>
 						<Contact path="/contact/" />
+<<<<<<< HEAD
 						<Others path="/others" /> 
 						<Menu path="/menu" /> 
 						<Notice path="/notice" /> 
+=======
+						<Members path="/members"/>
+>>>>>>> fyh
 						<ContactSuccess path="/contact/success" />
 						<NotFoundPage type="404" default />
 					</Router>
