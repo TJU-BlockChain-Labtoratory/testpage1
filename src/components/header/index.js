ecaaab5ec426c6478444f2381185e8d68d8de2ec
Van <1137623688@qq.com>
2020-04-25 17:16:54 +0800

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,11 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
-
+//网页菜单栏
 const Header = () => (
 	<div class={style.fixtop}>
 		<header class={style.header}>
+<<<<<<< HEAD
 			<Link href="/"><img src ="/assets/logo+white.png"/></Link>
 			<nav>
 			<Link activeClassName={style.active} href="/menu">Menu</Link>
@@ -16,6 +17,17 @@ const Header = () => (
 			</nav>
 		</header>
 	</div>
+=======
+			<Link href="/"><img src='/assets/logo+white.png' /></Link>
+			<nav>
+				<Link activeClassName={style.active} href="/blogs">公告</Link>
+				<Link activeClassName={style.active} href="/members">成员</Link>
+				<Link activeClassName={style.active} href="/contact">联系我们</Link>
+			</nav>
+		</header>
+	</div>
+	
+>>>>>>> fyh
 );
 
 export default Header;
