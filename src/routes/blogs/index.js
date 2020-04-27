3b34e18e8c1b6fe006dd0febe0f9f8ed7f357535
GoldenSwordClock <57226812+GoldenSwordClock@users.noreply.github.com>
2020-04-27 15:25:45 +0800

    Update index.js

--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -30,7 +30,7 @@ function getBlogsListing(data, isLoading) {
 		var notnews = new Array();
 		var j = 0;
 		for( var i = 0; i < blogs.edges.length ; i++ )
-		  if ( blogs.edges[i].details.tags.substr(1,blogs.edges[i].details.tags.length-1).split(',')[0] != 'news')
+		  if ( blogs.edges[i].details.tags.substr(0,blogs.edges[i].details.tags.length).split(',')[0] != 'news')
 		  { 
 	         notnews[j] = blogs.edges[i];
 		     j++;
@@ -45,7 +45,7 @@ function getBlogsListing(data, isLoading) {
 						<h2>{blog.details.title}</h2>
 						<div>
 							{
-								(blog.details.tags.substr(0, blog.details.tags.length - 1).split(',') || []).map(tag => <span class={style.tag}>{tag}</span>)
+								(blog.details.tags.substr(0, blog.details.tags.length).split(',') || []).map(tag => <span class={style.tag}>{tag}</span>)
 							}
 						</div>
 						<p class={style.preview}>
