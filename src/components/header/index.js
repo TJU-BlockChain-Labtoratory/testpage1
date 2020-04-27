65fe5580ca6a520c995be5b80957786e4418ca86
GoldenSwordClock <57226812+GoldenSwordClock@users.noreply.github.com>
2020-04-27 20:01:47 +0800

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,8 +8,7 @@ const Header = () => (
 			<img src ="/assets/logo+white.png"/>
 			<nav>
 				<Link activeClassName={style.active} href="/">首页</Link>
-				<Link activeClassName={style.active} href="/blogs">新闻</Link>
-				<Link activeClassName={style.active} href="/notice">公告</Link>
+				<Link activeClassName={style.active} href="/blogs">新闻公告</Link>
 				<Link activeClassName={style.active} href="/members">主要成员</Link>
 				<Link activeClassName={style.active} href="/others">其他</Link>
 				<Link activeClassName={style.active} href="/contact">联系我们</Link>
