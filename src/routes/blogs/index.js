136975bf8b428454e9f09c85b8fdb6b5776612ab
lslslscai <2052366734@qq.com>
2020-04-28 20:15:12 +0800

    Update index.js

--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -9,17 +9,13 @@ const blogs = (props) => {
 	<div>
 		<div class={style.pageNewsAndNotices}>
 			<h1 class={style.pageTitle}>新闻</h1>
-			{ getNewsListing(data, isLoading) }
+			{ getListing(data, isLoading) }
 		</div>
-	    <div class={style.pageNewsAndNotices}>
-			<h1 class={style.pageTitle}>公告</h1>
-			{ getNoticesListing(data, isLoading) }
-	    </div>
 	</div>
 	);
 };
 
-function getNewsListing(data, isLoading) {
+function getListing(data, isLoading) {
 	if (isLoading) {
 		return (
 			<article class={style.loadingPlaceholder}>
@@ -33,26 +29,15 @@ function getNewsListing(data, isLoading) {
 	console.log(data);
 	if (data && data.data) {
 		const { data: blogs } = data;
-		
-		var notnews = new Array();
-		var j = 0;
-		for( var i = 0; i < blogs.edges.length ; i++ )
-		  if ( blogs.edges[i].details.tags.substr(0,blogs.edges[i].details.tags.length).split(',') == 'news')
-		  { 
-	         notnews[j] = blogs.edges[i];
-		     j++;
-		  }
-		
-
 		return (
 			<>
-				{notnews.map(blog => (
+				{blogs.map(blog => (
 				<Link href={`/blog/${blog.id}`}>
 					<article class={style.block}>
 						<h2>{blog.details.title}</h2>
 						<div>
 							{
-								 <span class={style.tag}>{blog.details.time}</span>
+								(blog.details.tags.substr(1, blog.details.tags.length - 2).split(',') || []).map(tag => <span class={style.tag}>{tag}</span>)
 							}
 						</div>
 						<p class={style.preview}>
@@ -67,51 +52,4 @@ function getNewsListing(data, isLoading) {
 	}
 }
 
-function getNoticesListing(data, isLoading) {
-	if (isLoading) {
-		return (
-			<article class={style.loadingPlaceholder}>
-				<h2 class={`${style.blogtitle} loading`}>&nbsp;</h2>
-				<div class={`${style.loadingBody} loading`}>&nbsp;</div>
-				<div class={`${style.loadingBody} loading`}>&nbsp;</div>
-				<div class={`${style.loadingBody} loading`}>&nbsp;</div>
-			</article>
-		);
-	}
-	console.log(data);
-	if (data && data.data) {
-		const { data: blogs } = data;
-		
-		var notnews = new Array();
-		var j = 0;
-		for( var i = 0; i < blogs.edges.length ; i++ )
-		  if ( blogs.edges[i].details.tags.substr(0,blogs.edges[i].details.tags.length).split(',') != 'news')
-		  { 
-	         notnews[j] = blogs.edges[i];
-		     j++;
-		  }
-		
-
-		return (
-			<>
-				{notnews.map(blog => (
-				<Link href={`/blog/${blog.id}`}>
-					<article class={style.block}>
-						<h2>{blog.details.title}</h2>
-						<div>
-							{
-								 <span class={style.tag}>{blog.details.time}</span>
-							}
-						</div>
-						<p class={style.preview}>
-							{blog.preview}
-						</p>
-					</article>
-						
-				</Link>
-			))}
-			</>
-		);
-	}
-}
 export default blogs;
