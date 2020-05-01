bf0e60f43ea077a980e1ce9f8b5b5a26753dd7e9
lslslscai <2052366734@qq.com>
2020-05-01 23:23:39 +0800

    UI update

--- src/routes/achievement/index.js
+++ src/routes/achievement/index.js
@@ -7,7 +7,7 @@ const Achievement = (props) => {
 			<h1 class={style.pageTitle}>科研成果</h1>
 			<div class={style.formWrapper}>
 				<p class={style.pageBody}>
-					<div>你好</div>
+					<div>敬请期待</div>
 				</p>
 			</div>
 		</div>
